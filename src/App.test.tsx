d8c151edca2144c79819e931f24ea36f20995e01
Andres Cagua <af.cagua1011@gmail.com>
2023-03-14 08:35:53 -0500

    Add sorting results test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -58,7 +58,7 @@ describe("Score Board", () => {
 
     const startButton = screen.getByRole("button", { name: /start/i });
 
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
 
     await userEvent.click(startButton);
 
@@ -90,10 +90,10 @@ describe("Score Board", () => {
 
     await userEvent.type(homeTeamInput, "Colombia");
 
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
 
     expect(homeTeamInput).toHaveValue("Colombia");
-    expect(awayTeamInput).toHaveValue("Brasil");
+    expect(awayTeamInput).toHaveValue("Brazil");
     expect(startButton).toBeEnabled();
 
     await userEvent.click(startButton);
@@ -126,7 +126,7 @@ describe("Score Board", () => {
     const startButton = screen.getByRole("button", { name: /start/i });
 
     await userEvent.type(homeTeamInput, "Colombia");
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
     await userEvent.click(startButton);
 
     const [scoreHome, scoreAway] = screen.getAllByRole("button", {
@@ -135,13 +135,13 @@ describe("Score Board", () => {
 
     screen.getByRole("heading", { name: /playing/i, level: 2 });
 
-    screen.getByText(/Colombia 0 - 0 Brasil/);
+    screen.getByText(/Colombia 0 - 0 Brazil/);
     await userEvent.click(scoreHome);
-    screen.getByText(/Colombia 1 - 0 Brasil/);
+    screen.getByText(/Colombia 1 - 0 Brazil/);
     await userEvent.click(scoreHome);
-    screen.getByText(/Colombia 2 - 0 Brasil/);
+    screen.getByText(/Colombia 2 - 0 Brazil/);
     await userEvent.click(scoreAway);
-    screen.getByText(/Colombia 2 - 1 Brasil/);
+    screen.getByText(/Colombia 2 - 1 Brazil/);
 
     const finishGame = screen.getByRole("button", {
       name: /finish/i,
@@ -164,7 +164,7 @@ describe("Score Board", () => {
     const startButton = screen.getByRole("button", { name: /start/i });
 
     await userEvent.type(homeTeamInput, "Colombia");
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
     await userEvent.click(startButton);
 
     const finishGame = screen.getByRole("button", {
@@ -190,7 +190,7 @@ describe("Score Board", () => {
     const startButton = screen.getByRole("button", { name: /start/i });
 
     await userEvent.type(homeTeamInput, "Colombia");
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
     await userEvent.click(startButton);
 
     await userEvent.type(homeTeamInput, "Argentina");
@@ -229,7 +229,7 @@ describe("Score Board", () => {
     const startButton = screen.getByRole("button", { name: /start/i });
 
     await userEvent.type(homeTeamInput, "Colombia");
-    await userEvent.type(awayTeamInput, "Brasil");
+    await userEvent.type(awayTeamInput, "Brazil");
     await userEvent.click(startButton);
 
     await userEvent.type(homeTeamInput, "Argentina");
@@ -253,5 +253,128 @@ describe("Score Board", () => {
     });
 
     expect(finishGamesAfterClean).toHaveLength(0);
+  });
+
+  it("Should sort finished games by total score and then by most recent game", async () => {
+    render(<App />);
+
+    const homeTeamInput = screen.getByRole("textbox", { name: /home team/i });
+    const awayTeamInput = screen.getByRole("textbox", { name: /away team/i });
+    const startButton = screen.getByRole("button", { name: /start/i });
+
+    await userEvent.type(homeTeamInput, "Mexico");
+    await userEvent.type(awayTeamInput, "Canada");
+    await userEvent.click(startButton);
+
+    await userEvent.type(homeTeamInput, "Spain");
+    await userEvent.type(awayTeamInput, "Brazil");
+    await userEvent.click(startButton);
+
+    await userEvent.type(homeTeamInput, "Germany");
+    await userEvent.type(awayTeamInput, "France");
+    await userEvent.click(startButton);
+
+    await userEvent.type(homeTeamInput, "Uruguay");
+    await userEvent.type(awayTeamInput, "Italy");
+    await userEvent.click(startButton);
+
+    await userEvent.type(homeTeamInput, "Argentina");
+    await userEvent.type(awayTeamInput, "Australia");
+    await userEvent.click(startButton);
+
+    const [
+      ,
+      canadaScore,
+      spainScore,
+      brazilScore,
+      germanyScore,
+      franceScore,
+      uruguayScore,
+      italyScore,
+      argentinaScore,
+      australiaScore,
+    ] = screen.getAllByRole("button", {
+      name: /score/i,
+    });
+
+    const [
+      mexCanFinish,
+      espBraFinish,
+      gerFraFinish,
+      uruItaFinish,
+      argAusFinish,
+    ] = screen.getAllByRole("button", {
+      name: /finish/i,
+    });
+
+    await userEvent.click(australiaScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(canadaScore);
+    await userEvent.click(argentinaScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(canadaScore);
+    await userEvent.click(canadaScore);
+    await userEvent.click(brazilScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(argentinaScore);
+    await userEvent.click(canadaScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(argentinaScore);
+    await userEvent.click(canadaScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(germanyScore);
+    await userEvent.click(franceScore);
+    await userEvent.click(spainScore);
+    await userEvent.click(brazilScore);
+    await userEvent.click(spainScore);
+
+    //Finish a set of matches
+    await userEvent.click(mexCanFinish);
+    await userEvent.click(espBraFinish);
+    await userEvent.click(argAusFinish);
+
+    await userEvent.click(germanyScore);
+    await userEvent.click(franceScore);
+    await userEvent.click(uruguayScore);
+    await userEvent.click(uruguayScore);
+    await userEvent.click(uruguayScore);
+    await userEvent.click(uruguayScore);
+    await userEvent.click(uruguayScore);
+    await userEvent.click(uruguayScore);
+
+    await userEvent.click(italyScore);
+    await userEvent.click(italyScore);
+    await userEvent.click(italyScore);
+    await userEvent.click(italyScore);
+    await userEvent.click(italyScore);
+    await userEvent.click(italyScore);
+
+    await userEvent.click(uruItaFinish);
+    await userEvent.click(gerFraFinish);
+
+    const playingHeading = screen.queryByRole("heading", {
+      name: /playing/i,
+      level: 2,
+    });
+
+    expect(playingHeading).not.toBeInTheDocument();
+
+    const resultsHeading = screen.getByRole("heading", {
+      name: /results/i,
+      level: 2,
+    });
+    expect(resultsHeading).toBeVisible();
+
+    const resultList = screen.getAllByRole("listitem");
+    screen.logTestingPlaygroundURL();
+
+    expect(resultList[0]).toHaveTextContent("Uruguay 6 - 6 Italy");
+    expect(resultList[1]).toHaveTextContent("Spain 10 - 2 Brazil");
+    expect(resultList[2]).toHaveTextContent("Mexico 0 - 5 Canada");
+    expect(resultList[3]).toHaveTextContent("Argentina 3 - 1 Australia");
+    expect(resultList[4]).toHaveTextContent("Germany 2 - 2 France");
   });
 });
